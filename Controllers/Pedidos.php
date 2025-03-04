ea1ab59ac1107f3de31dc6e90cffbd032ade7d3a
FDMC04 <franciscodanielmzca@gmail.com>
2025-03-04 15:27:12 -0600

    Generar PDF del pedido
    Se agrego el método para generar pdfs

--- Controllers/Pedidos.php
+++ Controllers/Pedidos.php
@@ -52,7 +52,8 @@
 
 					if($_SESSION['permisosMod']['r']){
 						$btnView .= '<a title="Ver Detalle" href="'.base_url().'/pedidos/orden/'.$arrData[$i]['idpedido'].'" target="_blanck" class="btn btn-info btn-sm"> <i class="far fa-eye"></i> </a>
-                                    <button class="btn btn-danger btn-sm" onClick="fntViewPDF('.$arrData[$i]['idpedido'].')" title="Generar PDF"><i class="fas fa-file-pdf"></i></button> ';
+
+						<a title="Generar PDF" href="'.base_url().'/factura/generarFactura/'.$arrData[$i]['idpedido'].'" target="_blanck" class="btn btn-danger btn-sm"> <i class="far fa-file-pdf"></i> </a> ';
                         if($arrData[$i]['idtipopago'] == 1){
                             $btnView .= '<a title="Ver Transacción" href="'.base_url().'/pedidos/transaccion/'.$arrData[$i]['idtransaccionpaypal'].'" target="_blanck" class="btn btn-info btn-sm"> <i class="fa fa-paypal" aria-hidden="true"></i> </a> ';
                         }else{
@@ -61,9 +62,6 @@
 					}
 					if($_SESSION['permisosMod']['u']){
 						$btnEdit = '<button class="btn btn-primary btn-sm " onClick="fntEditInfo(this,'.$arrData[$i]['idpedido']. ')" title="Editar pedido"><i class="fa-solid fa-pencil"></i></button>';
-					}
-					if($_SESSION['permisosMod']['d']){
-						$btnDelete = '<button class="btn btn-danger btn-sm " onClick="fntDelInfo('.$arrData[$i]['idpedido']. ')" title="Eliminar pedido"><i class="fa-solid fa-trash-alt"></i></button> ';
 					}
 
 					$arrData[$i]['options'] = '<div class="text-center">'.$btnView.' '.$btnEdit.' '. $btnDelete.'</div>';
